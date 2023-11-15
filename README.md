582dd8efda0138d03042bf3d833702436015ac8f
devalgas <kamgadevalgas@gmail.com>
2023-11-15 23:05:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Build an Angular 15 CRUD example App to consume Rest APIs, display, modify & search data.
+
+Tutorial Application in that:
+- Each Tutorial has id, title, description, published status.
+- We can create, retrieve, update, delete Tutorials.
+- There is a Search bar for finding Tutorials by title.
---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
+import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
+
+import { provideClarity } from 'ngx-clarity';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddTutorialComponent,
+    TutorialDetailsComponent,
+    TutorialsListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    provideClarity({
+          enabled: true,
+          projectId: 'jr3brvonpm',
+        }),
+    ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
